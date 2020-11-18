f2cd75b43ec45f4911d6482b2adb4c783ecdea1a
feijie <feijie89@qq.com>
2020-11-18 21:13:36 +0800

    完善crud handler

--- src/Vertex.Grain.EntityFramework/CrudHandler{TPrimaryKey,TSnapshot}.cs
+++ src/Vertex.Grain.EntityFramework/CrudHandler{TPrimaryKey,TSnapshot}.cs
@@ -16,53 +16,39 @@ namespace Vertex.Grain.EntityFramework
 
         public CrudHandler(IMapper mapper)
         {
-            Mapper = mapper;
+            this.Mapper = mapper;
         }
 
-        //public override void Apply(SnapshotUnit<TPrimaryKey, TSnapshot> snapshotBox, EventUnit<TPrimaryKey> eventBox)
-        //{
-
-        //}
-
-        //public override void CustomApply(Snapshot<TPrimaryKey, TSnapshot> snapshot, FullyEvent<TPrimaryKey> fullyEvent)
-        //{
-        //    Apply(snapshot.State, fullyEvent.Event);
-        //}
+        public override void Apply(SnapshotUnit<TPrimaryKey, TSnapshot> snapshotBox, EventUnit<TPrimaryKey> eventBox)
+        {
+            switch (eventBox.Event)
+            {
+                case CreatingSnapshotEvent<TSnapshot>:
+                    return;
+                case UpdatingSnapshotEvent<TSnapshot>:
+                    return;
+                case DeletingSnapshotEvent<TSnapshot>:
+                    return;
+                default:
+                    base.Apply(snapshotBox, eventBox);
+                    return;
+            }
+        }
 
         public void CreatingSnapshotHandle(TSnapshot snapshotState, CreatingSnapshotEvent<TSnapshot> evt)
         {
-            Mapper.Map(evt.Snapshot, snapshotState);
+            this.Mapper.Map(evt.Snapshot, snapshotState);
         }
 
         public void UpdatingSnapshotHandle(TSnapshot snapshotState, UpdatingSnapshotEvent<TSnapshot> evt)
         {
-            Mapper.Map(evt.Snapshot, snapshotState);
+            this.Mapper.Map(evt.Snapshot, snapshotState);
         }
 
         public void DeletingSnapshotHandle(TSnapshot snapshotState, DeletingSnapshotEvent<TPrimaryKey> evt)
         {
             var defaultSnapshot = new TSnapshot();
-            Mapper.Map(defaultSnapshot, snapshotState);
+            this.Mapper.Map(defaultSnapshot, snapshotState);
         }
-
-        #region Implementation of ICrudHandle<in TSnapshot>
-
-        public virtual void Apply(TSnapshot snapshot, IEvent @event)
-        {
-            switch (@event)
-            {
-                case CreatingSnapshotEvent<TSnapshot> evt:
-                    CreatingSnapshotHandle(snapshot, evt);
-                    break;
-                case UpdatingSnapshotEvent<TSnapshot> evt:
-                    UpdatingSnapshotHandle(snapshot, evt);
-                    break;
-                case DeletingSnapshotEvent<TPrimaryKey> evt:
-                    DeletingSnapshotHandle(snapshot, evt);
-                    break;
-            }
-        }
-
-        #endregion
     }
 }

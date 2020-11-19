06fea1f52742eafee639f89cac37919aaba6b68f
feijie <feijie89@qq.com>
2020-11-19 10:44:58 +0800

    完成crudGrain的扩展方法

---
+++ src/Vertex.Grain.EntityFramework/ServiceCollectionServiceExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Reflection;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Vertex.Abstractions.Snapshot;
+using Vertex.Grain.EntityFramework.Abstractions;
+
+namespace Vertex.Grain.EntityFramework
+{
+    public static class ServiceCollectionServiceExtensions
+    {
+        public static IServiceCollection AddCrudGrain(this IServiceCollection serviceCollection, Assembly[] assemblies, Action<IMapperConfigurationExpression> configAction = null)
+        {
+            serviceCollection.AddTransient(typeof(ICrudHandler<,>), typeof(CrudHandler<,>));
+            serviceCollection.AddSingleton(typeof(ISnapshotHandler<,>), typeof(CrudHandler<,>));
+            serviceCollection.AddAutoMapper(configAction, assemblies);
+            return serviceCollection;
+        }
+    }
+}
